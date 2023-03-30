ea1f9b2b57c2e33b677a3cfcc98678ffcc4f47a1
sanojfcr <masatoshi.sano.jfcr@gmail.com>
2023-03-30 14:44:40 +0900

    add link shell script

---
+++ ln_dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/.bash_profile.login ~/
+ln -sf ~/dotfiles/.bash_profile.intr ~/
+ln -sf ~/dotfiles/.bashrc.login ~/
+ln -sf ~/dotfiles/.bashrc.intr ~/
+ln -sf ~/dotfiles/.inputrc ~/
